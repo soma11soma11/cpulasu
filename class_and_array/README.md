fce5d2c67d740deb9bc3fb9c09e337c1484eeb3c
soma11soma11 <h2so4cos0@gmail.com>
2017-09-23 18:02:15 +0100

    class

---
+++ class_and_array/README.md
@@ -0,0 +1,6 @@
+For this program, practice using classes by :
+
+using the Dogs class to create an animal shelter roster.
+add a constructor to the Dogs class. The initial name for a Dog is "Unknown"
+declare and define the function:
+printRoster(roster,SIZE);
---
+++ class_and_array/main.cpp
@@ -0,0 +1,19 @@
+/*Goal: practice using the Dogs class.
+ **Create an array called roster whose elements are of the type Dogs.
+ */
+
+#include "main.hpp"
+
+
+int main()
+{
+    const int SIZE=3;
+    Dog roster[SIZE];
+    
+    roster[0].setName("Blue");
+    roster[1].setName("King");
+    roster[2].setName("Spot");
+    
+    printRoster(roster,SIZE);
+    return 0;
+}
